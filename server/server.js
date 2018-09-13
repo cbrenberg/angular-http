cb6f965a9b26a72d2e30e0a99c712cbf755f98dd
Christopher Brenberg <cbrenberg@gmail.com>
2018-09-13 10:26:50 -0500

    serve files from node express server

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(express.static('server/public'))
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log('listening on port:', PORT)
+});
