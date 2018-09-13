50004248539f859187b934c418f492a9845e394c
Christopher Brenberg <cbrenberg@gmail.com>
2018-09-13 11:06:13 -0500

    uses angular controller

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,11 @@
 console.log('client.js');
+
+const shellApp = angular.module('ShellApp', []);
+
+shellApp.controller('ShellController', ['$http', function($http) {
+  console.log('angularHttpController loaded');
+
+  const self = this;
+  self.number = 7;
+
+}]);
--- server/server.js
+++ server/server.js
@@ -8,5 +8,5 @@ app.use(express.static('server/public'))
 const PORT = 5000;
 
 app.listen(PORT, () => {
-  console.log('listening on port:', PORT)
+  console.log(`listening on port ${PORT}`)
 });
